d7fb287b99fd59c7283a65e087d4c52d8adc7237
Ryan Kwok <kwokryan11@gmail.com>
2026-02-05 03:53:19 -0500

    changed web name

--- api/src/main.ts
+++ api/src/main.ts
@@ -19,7 +19,13 @@ const allowedOrigins = process.env.CORS_ORIGINS?.split(",").map(origin => origin
 
 app.use(
   cors({
-    origin: "https://slides-ot.netlify.app",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
